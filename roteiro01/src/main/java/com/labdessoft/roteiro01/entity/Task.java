d3215fbbe4c53ded45fab482a28f8bbb7af4dd65
ana.gomes <analuizasg52@gmail.com>
2024-04-20 12:30:46 -0300

    adicionando novas regras e correção de erros

--- roteiro01/src/main/java/com/labdessoft/roteiro01/entity/Task.java
+++ roteiro01/src/main/java/com/labdessoft/roteiro01/entity/Task.java
@@ -1,30 +1,45 @@
-package com.labdessoft.roteiro01.entity.Task;
+package com.labdessoft.roteiro01.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import java.time.LocalDate;
+import com.labdessoft.roteiro01.entity.TaskType;
 @Setter
 @Getter
 @Schema(description = "Todos os detalhes sobre uma tarefa. ")
+@Entity
 public class Task {
 
     // Getters e Setters
+    @Setter
+    @Id
     private Long id; @Schema(description = "Id da tarefa. ")
     private String title; @Schema(description = "Título da tarefa. ")
 
     private String description;@Schema(description = "Todos os detalhes sobre uma tarefa. ")
     private boolean completed;
+    @Schema(description = "Data prevista de conclusão da tarefa.")
+    private LocalDate dueDate;
+    @Schema(description = "Prazo previsto de conclusão da tarefa (em dias).")
+    private Integer dueDays;
+    @Schema(description = "Tipo de tarefa (Data, Prazo, Livre).")
+    private TaskType taskType;
+
 
     public Task() {
     }
 
-    public Task(String title, String description) {
+    public Task(String title, String description, TaskType taskType) {
         this.title = title;
         this.description = description;
+        this.taskType = taskType;
         this.completed = false;
+
     }
 
     public Long getId() {
@@ -65,6 +80,5 @@ public class Task {
         return "Task [id=" + id + ", description=" + description + ", completed=" +
                 completed + "]";
     }
-
 
 }
