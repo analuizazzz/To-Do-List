cb627b5164adc5c70b62021c4aaec3e4c4267295
ana.gomes <analuizasg52@gmail.com>
2024-05-21 20:48:41 -0300

    correções e testes unitários

--- roteiro01/src/main/java/com/labdessoft/Roteiro01Application.java
+++ roteiro01/src/main/java/com/labdessoft/Roteiro01Application.java
@@ -1,4 +1,4 @@
-package com.labdessoft.roteiro01;
+package com.labdessoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- roteiro01/src/test/java/com/labdessoft/roteiro01/integration/TaskControllerIntegrationTest.java
+++ roteiro01/src/test/java/com/labdessoft/roteiro01/integration/TaskControllerIntegrationTest.java
@@ -1,20 +1,22 @@
+package com.labdessoft.roteiro01.integration;
+
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
+// import org.junit.platform.runner.JUnitPlatform;
+// import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import com.labdessoft.roteiro01.Roteiro01Application;
+import com.labdessoft.Roteiro01Application;
 
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.Matchers.equalTo;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
+// @RunWith(JUnitPlatform.class)
 @SpringBootTest(classes = {Roteiro01Application.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public class TaskControllerIntegrationTest {
